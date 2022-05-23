2529b23dfb313284f64b8da6d7001330113c115c
Diego Sisternes <daigos.sd@gmail.com>
2022-05-22 19:07:16 -0500

    Sidenav menu has been implemented

--- resources/views/layouts/general.blade.php
+++ resources/views/layouts/general.blade.php
@@ -31,6 +31,134 @@
 
     <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
 <style>  
+@import url(//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css);
+}
+@import url(https://fonts.googleapis.com/css?family=Titillium+Web:300);
+.fa-2x {
+font-size: 2em;
+}
+.fa {
+position: relative;
+display: table-cell;
+width: 60px;
+height: 60px;
+text-align: center;
+vertical-align: middle;
+font-size:20px;
+}
+
+
+.main-menu:hover,nav.main-menu.expanded {
+width:245px;
+overflow:visible;
+}
+
+.main-menu {
+background:#9AD1D4;
+border-top:1px solid #099dca;
+border-right:1px solid #099dca;
+border-bottom:1px solid #099dca;
+position:absolute;
+top:auto;
+bottom:auto;
+height:auto;
+left:0;
+width:60px;
+overflow:hidden;
+-webkit-transition:width .05s linear;
+transition:width .05s linear;
+-webkit-transform:translateZ(0) scale(1,1);
+z-index:1000;
+}
+
+.main-menu>ul {
+margin:7px 0;
+}
+
+.main-menu li {
+position:relative;
+display:block;
+width:245px;
+}
+
+.main-menu li>a {
+position:relative;
+display:table;
+border-collapse:collapse;
+border-spacing:0;
+color:rgb(0, 0, 0);
+ font-family: arial;
+font-size: 14px;
+text-decoration:none;
+-webkit-transform:translateZ(0) scale(1,1);
+-webkit-transition:all .1s linear;
+transition:all .1s linear;
+  
+}
+
+.main-menu .nav-icon {
+position:relative;
+display:table-cell;
+width:60px;
+height:36px;
+text-align:center;
+vertical-align:middle;
+font-size:18px;
+}
+
+.main-menu .nav-text {
+position:relative;
+display:table-cell;
+vertical-align:middle;
+width:190px;
+  font-family: 'Titillium Web', sans-serif;
+}
+
+.no-touch .scrollable.hover {
+overflow-y:hidden;
+}
+
+.no-touch .scrollable.hover:hover {
+overflow-y:auto;
+overflow:visible;
+}
+
+a:hover,a:focus {
+text-decoration:none;
+}
+
+nav {
+-webkit-user-select:none;
+-moz-user-select:none;
+-ms-user-select:none;
+-o-user-select:none;
+user-select:none;
+}
+
+nav ul,nav li {
+outline:0;
+margin:0;
+padding:0;
+}
+
+.main-menu li:hover>a,nav.main-menu li.active>a,.dropdown-menu>li>a:hover,.dropdown-menu>li>a:focus,.dropdown-menu>.active>a,.dropdown-menu>.active>a:hover,.dropdown-menu>.active>a:focus,.no-touch .dashboard-page nav.dashboard-menu ul li:hover a,.dashboard-page nav.dashboard-menu ul li.active a {
+color:#fff;
+background-color:#5fa2db;
+}
+
+.area {
+float: left;
+background: #e2e2e2;
+width: 100%;
+height: 100%;
+}
+@font-face {
+  font-family: 'Titillium Web';
+  font-style: normal;
+  font-weight: 300;
+  src: local('Titillium WebLight'), local('TitilliumWeb-Light'), url(http://themes.googleusercontent.com/static/fonts/titilliumweb/v2/anMUvcNT0H1YN4FII8wpr24bNCNEoFTpS2BTjF6FB5E.woff) format('woff');
+}
+
 .navbar-blue{
   background:#9AD1D4;
 } 
@@ -70,17 +198,6 @@
               </button>
 
               <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                  <!-- Left Side Of Navbar -->
-                  <ul class="navbar-nav me-auto">
-                    <div class="navbar-nav">
-                      <a class="nav-link active" aria-current="page" href="#">Gestor de Reservaciones</a>
-                      <a class="nav-link" href="#">Trabajadores</a>
-                      <a class="nav-link" href="#">Clientes</a>
-                      <a class="nav-link" href="#">Pagos y Adeudos</a>
-                      <a class="nav-link" href="#">Habitaciones</a>
-                      <a class="nav-link" href="#">Ocupación del Hotel</a>
-                    </div>
-                  </ul>
 
                   <!-- Right Side Of Navbar -->
                   <ul class="navbar-nav ms-auto">
@@ -106,8 +223,8 @@
                               <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                                   <a class="dropdown-item" href="{{ route('logout') }}"
                                      onclick="event.preventDefault();
-                                                   document.getElementById('logout-form').submit();">
-                                      {{ __('Logout') }}
+                                                   document.getElementById('logout-form').submit();"><i class="fa fa-power-off fa-2x"> {{ __('Logout') }}</i>
+                                      
                                   </a>
 
                                   <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
@@ -122,9 +239,61 @@
       </nav>
      </div>
 </head>
-<section
-      class="p-4 border-bottom "
-    >
+<div class="area"></div><nav class="main-menu">
+  <ul><li>
+          <a href="#!">
+              <i class="fa fa-calendar fa-2x"></i>
+              <span class="nav-text">
+                Gestor de Reservaciones
+              </span>
+          </a>
+        
+      </li>
+      <li class="has-subnav">
+          <a href="#">
+              <i class="fa fa-user fa-2x"></i>
+              <span class="nav-text">
+                Personal del Hotel
+              </span>
+          </a>
+          
+      </li>
+      <li class="has-subnav">
+          <a href="#">
+             <i class="fa fa-users fa-2x"></i>
+              <span class="nav-text">
+                Clientes
+              </span>
+          </a>
+          
+      </li>
+      <li class="has-subnav">
+          <a href="#">
+             <i class="fa fa-btc fa-2x"></i>
+              <span class="nav-text">
+                Pagos y Adeudos
+              </span>
+          </a>
+         
+      </li>
+      <li>
+          <a href="#">
+              <i class="fa fa-building-o fa-2x"></i>
+              <span class="nav-text">
+                Habitaciones
+              </span>
+          </a>
+      </li>
+      <li>
+          <a href="#">
+              <i class="fa fa-bar-chart-o fa-2x"></i>
+              <span class="nav-text">
+                Ocupación del Hotel
+              </span>
+          </a>
+      </li>
+</nav>
+<section class="p-4 border-bottom ">
 <body class="body text-center text-lg-start text-muted">
     <div id="app">
 
